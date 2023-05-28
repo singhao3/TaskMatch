78ed1a3829d7f6e4a548533588591801378a8e4a
singhao <tiesinghao3300@gmail.com>
2023-05-28 17:52:09 +0800

    Added user role based singup and login

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:taskmatch/pages/home_page.dart';
 import 'package:taskmatch/widget_tree.dart';
 import 'package:firebase_core/firebase_core.dart';
+// import 'package:taskmatch/pages/task_doer_home_page.dart';
+// import 'package:taskmatch/pages/task_seeker_home_page.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +22,13 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const WidgetTree(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const WidgetTree(),
+        '/home': (context) => const HomePage(),
+        '/taskdoerhome': (context) => const HomePage(),
+        '/taskseekerhome': (context) => const HomePage(),
+      },
     );
   }
 }
--- lib/widget_tree.dart
+++ lib/widget_tree.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:taskmatch/auth.dart';
 import 'package:taskmatch/pages/home_page.dart';
 import 'package:taskmatch/pages/login_screen.dart';
@@ -13,18 +15,38 @@ class WidgetTree extends StatefulWidget {
 class _WidgetTreeState extends State<WidgetTree> {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-        stream: Auth().authStateChanges,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return const Center(child: Text('Something went wrong!'));
-          } else if (snapshot.hasData) {
-            return const HomePage();
+    return StreamBuilder<User?>(
+      stream: Auth().authStateChanges,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return const Center(child: Text('Something went wrong!'));
+        } else if (snapshot.hasData) {
+          final user = snapshot.data;
+          if (user != null) {
+            return FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+              future: FirebaseFirestore.instance
+                  .collection('task_doers')
+                  .doc(user.uid)
+                  .get(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData && snapshot.data!.exists) {
+                  // User is a task-doer, navigate to task-doer home page
+                  return const HomePage();
+                } else {
+                  // User is not a task-doer, navigate to task-seeker home page
+                  return const HomePage();
+                }
+              },
+            );
           } else {
             return const LoginScreen();
           }
-        });
+        } else {
+          return const LoginScreen();
+        }
+      },
+    );
   }
 }
