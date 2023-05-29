43c27a870c3f865656fea6f26b11d300a81deaaa
singhao <tiesinghao3300@gmail.com>
2023-05-30 00:47:11 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'package:taskmatch/pages/taskseeker/taskseeker_homepage.dart';
 import 'package:taskmatch/pages/taskdoer/taskdoer_homepage.dart';
 import 'package:taskmatch/widget_tree.dart';
 import 'package:firebase_core/firebase_core.dart';
-// import 'package:taskmatch/pages/task_doer_home_page.dart';
-// import 'package:taskmatch/pages/task_seeker_home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
