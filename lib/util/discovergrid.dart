6870b7b54add730d0234123a8ec54be1730f2e9e
singhao <tiesinghao3300@gmail.com>
2023-05-30 11:04:51 +0800

    Update discovergrid.dart

--- lib/util/discovergrid.dart
+++ lib/util/discovergrid.dart
@@ -1,221 +1,21 @@
-// import 'package:flutter/material.dart';
-
-// class discovergrid extends StatelessWidget {
-//   const discovergrid({Key? key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListView.builder(
-//       itemCount: 10,
-//       itemBuilder: (context, index) {
-//         return Padding(
-//           padding: const EdgeInsets.all(8.0),
-//           child: Card(
-//             child: Padding(
-//               padding: const EdgeInsets.all(8.0),
-//               child: Row(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: [
-//                   Expanded(
-//                     child: Column(
-//                       crossAxisAlignment: CrossAxisAlignment.start,
-//                       children: [
-//                         Text(
-//                           'Project Name $index',
-//                           style: TextStyle(
-//                             fontWeight: FontWeight.bold,
-//                             fontSize: 16,
-//                           ),
-//                         ),
-//                         const SizedBox(height: 4),
-//                         Text(
-//                           'Description $index',
-//                           style: TextStyle(fontSize: 14),
-//                         ),
-//                         const SizedBox(height: 4),
-//                         Text(
-//                           'UNiversity name $index',
-//                           style: TextStyle(
-//                             fontSize: 14,
-//                             color: Colors.grey,
-//                           ),
-//                         ),
-//                       ],
-//                     ),
-//                   ),
-//                   Text(
-//                     '\$10.00',
-//                     style: TextStyle(
-//                       fontWeight: FontWeight.bold,
-//                       fontSize: 16,
-//                       color: Colors.green,
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           ),
-//         );
-//       },
-//     );
-//   }
-// }
-// import 'package:flutter/material.dart';
-
-// class DiscoverGrid extends StatelessWidget {
-//   const DiscoverGrid({Key? key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListView.builder(
-//       itemCount: 10,
-//       itemBuilder: (context, index) {
-//         return Padding(
-//           padding: const EdgeInsets.all(8.0),
-//           child: Card(
-//             child: Padding(
-//               padding: const EdgeInsets.all(8.0),
-//               child: Row(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: [
-//                   Expanded(
-//                     child: Column(
-//                       crossAxisAlignment: CrossAxisAlignment.start,
-//                       children: [
-//                         Text(
-//                           'Project Name $index',
-//                           style: TextStyle(
-//                             fontWeight: FontWeight.bold,
-//                             fontSize: 16,
-//                           ),
-//                         ),
-//                         const SizedBox(height: 4),
-//                         Text(
-//                           'Description $index',
-//                           style: TextStyle(fontSize: 14),
-//                         ),
-//                         const SizedBox(height: 4),
-//                         Text(
-//                           'University name $index',
-//                           style: TextStyle(
-//                             fontSize: 14,
-//                             color: Colors.grey,
-//                           ),
-//                         ),
-//                       ],
-//                     ),
-//                   ),
-//                   Text(
-//                     '\$10.00',
-//                     style: TextStyle(
-//                       fontWeight: FontWeight.bold,
-//                       fontSize: 16,
-//                       color: Colors.green,
-//                     ),
-//                   ),
-//                   IconButton(
-//                     icon: Icon(Icons.favorite_border),
-//                     onPressed: () {
-//                       // TODO: Add project to wishlist
-//                     },
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           ),
-//         );
-//       },
-//     );
-//   }
-// }
-// import 'package:flutter/material.dart';
-
-// class DiscoverGrid extends StatelessWidget {
-//   const DiscoverGrid({Key? key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListView.builder(
-//       itemCount: 10,
-//       itemBuilder: (context, index) {
-//         return Padding(
-//           padding: const EdgeInsets.all(8.0),
-//           child: Card(
-//             child: Padding(
-//               padding: const EdgeInsets.all(8.0),
-//               child: Row(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: [
-//                   Expanded(
-//                     child: Column(
-//                       crossAxisAlignment: CrossAxisAlignment.start,
-//                       children: [
-//                         Text(
-//                           'Project Name $index',
-//                           style: TextStyle(
-//                             fontWeight: FontWeight.bold,
-//                             fontSize: 16,
-//                           ),
-//                         ),
-//                         const SizedBox(height: 4),
-//                         Text(
-//                           'Description $index',
-//                           style: TextStyle(fontSize: 14),
-//                         ),
-//                         const SizedBox(height: 4),
-//                         Text(
-//                           'University name $index',
-//                           style: TextStyle(
-//                             fontSize: 14,
-//                             color: Colors.grey,
-//                           ),
-//                         ),
-//                       ],
-//                     ),
-//                   ),
-//                   Text(
-//                     '\$10.00',
-//                     style: TextStyle(
-//                       fontWeight: FontWeight.bold,
-//                       fontSize: 16,
-//                       color: Colors.green,
-//                     ),
-//                   ),
-//                   IconButton(
-//                     icon: Icon(Icons.favorite_border),
-//                     onPressed: () {
-//                       // Add the item to the wishlist
-//                       // You can implement your desired logic here
-//                       // For simplicity, let's just print a message
-//                       print('Added to wishlist: Project $index');
-//                     },
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           ),
-//         );
-//       },
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class DiscoverGrid extends StatefulWidget {
-  const DiscoverGrid({Key? key});
+class DiscoverGrid extends StatelessWidget {
+  final List<QueryDocumentSnapshot> documents;
 
-  @override
-  _DiscoverGridState createState() => _DiscoverGridState();
-}
-
-class _DiscoverGridState extends State<DiscoverGrid> {
-  List<bool> isFavoriteList = List.generate(10, (index) => false);
+  const DiscoverGrid({Key? key, required this.documents}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: 10,
+      itemCount: documents.length,
       itemBuilder: (context, index) {
+        final document = documents[index];
+        final title = document['title'] ?? '';
+        final description = document['description'] ?? '';
+        final budget = document['budget'] ?? '';
+
         return Padding(
           padding: const EdgeInsets.all(8.0),
           child: Card(
@@ -229,46 +29,28 @@ class _DiscoverGridState extends State<DiscoverGrid> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          'Project Name $index',
-                          style: TextStyle(
+                          title,
+                          style: const TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 16,
                           ),
                         ),
                         const SizedBox(height: 4),
                         Text(
-                          'Description $index',
-                          style: TextStyle(fontSize: 14),
+                          description,
+                          style: const TextStyle(fontSize: 14),
                         ),
                         const SizedBox(height: 4),
-                        Text(
-                          'University name $index',
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.grey,
-                          ),
-                        ),
                       ],
                     ),
                   ),
                   Text(
-                    '\$10.00',
-                    style: TextStyle(
+                    '\$$budget',
+                    style: const TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 16,
                       color: Colors.green,
                     ),
-                  ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.favorite,
-                      color: isFavoriteList[index] ? Colors.red : Colors.grey,
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        isFavoriteList[index] = !isFavoriteList[index];
-                      });
-                    },
                   ),
                 ],
               ),
