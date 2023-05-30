f1ec4cce9b01902c79f492fc506083f006bbf7ff
singhao <tiesinghao3300@gmail.com>
2023-05-30 14:21:10 +0800

    Update rootpage.dart

--- lib/pages/profilepage/rootpage.dart
+++ lib/pages/profilepage/rootpage.dart
@@ -1,43 +1,10 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:taskmatch/auth.dart';
 import 'package:taskmatch/pages/profilepage/changepassword.dart';
+import 'package:taskmatch/pages/profilepage/customform.dart';
 import 'package:taskmatch/pages/profilepage/update_profile_page.dart';
 
-class RootPage extends StatefulWidget {
-  RootPage({Key? key}) : super(key: key);
-
-  @override
-  _RootPageState createState() => _RootPageState();
-}
-
-class _RootPageState extends State<RootPage> {
-  User? user = Auth().currentUser;
-  String? username;
-  String? phoneNumber;
-
-  @override
-  void initState() {
-    super.initState();
-    // Call a function to fetch the user ID and email from Firebase
-    fetchUserData();
-  }
-
-  Future<void> fetchUserData() async {
-    final uid = user?.uid;
-    if (uid != null) {
-      final snapshot =
-          await FirebaseFirestore.instance.collection('task_doers').doc(uid).get();
-      final data = snapshot.data();
-      if (data != null) {
-        setState(() {
-          username = data['name'] ?? ''; 
-          phoneNumber = data['phone_number'] ?? '';// Update the username state variable
-        });
-      }
-    }
-  }
+class RootPage extends StatelessWidget {
+  const RootPage({Key? key}) : super(key: key);
 
   void navigateToUpdateProfile(BuildContext context) {
     Navigator.push(
@@ -52,7 +19,7 @@ class _RootPageState extends State<RootPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.white,
+        color: Colors.white, 
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
@@ -60,11 +27,11 @@ class _RootPageState extends State<RootPage> {
             Row(
               children: [
                 Text(
-                  'Profile',
+                  '    Profile',
                   style: TextStyle(
                     fontSize: 24,
                     fontWeight: FontWeight.bold,
-                    color: Colors.black,
+                    color: Colors.black, 
                   ),
                 ),
                 const Spacer(),
@@ -72,9 +39,9 @@ class _RootPageState extends State<RootPage> {
                   padding: const EdgeInsets.symmetric(horizontal: 16.0),
                   child: Container(
                     decoration: BoxDecoration(
-                      color: Colors.green,
+                      color: Colors.white,
                       border: Border.all(
-                        color: Colors.white,
+                        color: Colors.green,
                         width: 2.0,
                       ),
                       borderRadius: BorderRadius.circular(10.0),
@@ -102,61 +69,17 @@ class _RootPageState extends State<RootPage> {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      TextFormField(
-                        initialValue: username,
-                        decoration: InputDecoration(
-                          icon: Icon(Icons.person, color: Colors.green),
-                          labelText: 'Name',
-                          labelStyle: TextStyle(color: Colors.green),
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.green),
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.green),
-                          ),
-                        ),
-                        style: TextStyle(color: Colors.green),
-                      ),
-                      SizedBox(height: 16),
-                      TextFormField(
-                        initialValue: user?.email,
-                        readOnly: true,
-                        decoration: InputDecoration(
-                          icon: Icon(Icons.email, color: Colors.green),
-                          labelText: 'Email Address',
-                          labelStyle: TextStyle(color: Colors.green),
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.green),
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.green),
-                          ),
-                        ),
-                        style: TextStyle(color: Colors.green),
-                      ),
-                      TextFormField(
-              initialValue: phoneNumber,
-              readOnly: true, // Make the field read-only
-              decoration: InputDecoration(
-                icon: Icon(Icons.phone, color: Colors.green),
-                labelText: 'Phone Number',
-                labelStyle: TextStyle(color: Colors.green),
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.green),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.green),
-                ),
-              ),
-              style: TextStyle(color: Colors.green),
-            ),
+                      MyCustomForm(),
                       SizedBox(height: 20),
                       ElevatedButton(
                         onPressed: () => navigateToUpdateProfile(context),
                         style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 12.0),
-                          backgroundColor: Colors.green,
-                          foregroundColor: Colors.white,
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 32.0, vertical: 12.0),
+                          backgroundColor:
+                              Colors.green, 
+                          foregroundColor:
+                              Colors.white, 
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(8.0),
                           ),
@@ -169,14 +92,18 @@ class _RootPageState extends State<RootPage> {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-                              builder: (BuildContext context) => const ChangePassword(),
+                              builder: (BuildContext context) =>
+                                  const ChangePassword(),
                             ),
                           );
                         },
                         style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 12.0),
-                          primary: Colors.green,
-                          onPrimary: Colors.white,
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 32.0, vertical: 12.0),
+                          primary:
+                              Colors.green, // Set the button color to green
+                          onPrimary:
+                              Colors.white, // Set the text color to white
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(8.0),
                           ),
