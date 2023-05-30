90607a7508bfcfe73502b8032747ddd4ce72cf67
Lai Chee Yee <cylai2001@graduate.utm.my>
2023-05-30 13:37:00 +0800

    delete some search bar
    delet soem search bar

--- lib/pages/profilepage/rootpage.dart
+++ lib/pages/profilepage/rootpage.dart
@@ -1,127 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:taskmatch/pages/profilepage/changepassword.dart';
-// import 'package:taskmatch/pages/profilepage/customform.dart';
-// import 'package:taskmatch/pages/profilepage/update_profile_page.dart';
-
-// class RootPage extends StatelessWidget {
-//   const RootPage({Key? key}) : super(key: key);
-
-//   void navigateToUpdateProfile(BuildContext context) {
-//     Navigator.push(
-//       context,
-//       MaterialPageRoute(
-//         builder: (BuildContext context) => const UpdateProfile(),
-//       ),
-//     );
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Container(
-//         color: Colors.white, 
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.start,
-//           children: [
-//             SizedBox(height: 20),
-//             Row(
-//               children: [
-//                 Text(
-//                   '    Profile',
-//                   style: TextStyle(
-//                     fontSize: 24,
-//                     fontWeight: FontWeight.bold,
-//                     color: Colors.black, 
-//                   ),
-//                 ),
-//                 const Spacer(),
-//                 Padding(
-//                   padding: const EdgeInsets.symmetric(horizontal: 16.0),
-//                   child: Container(
-//                     decoration: BoxDecoration(
-//                       color: Colors.white,
-//                       border: Border.all(
-//                         color: Colors.green,
-//                         width: 2.0,
-//                       ),
-//                       borderRadius: BorderRadius.circular(10.0),
-//                     ),
-//                     child: Padding(
-//                       padding: const EdgeInsets.all(8.0),
-//                       child: Text(
-//                         'Task-seekers',
-//                         style: TextStyle(
-//                           fontSize: 16,
-//                           fontWeight: FontWeight.bold,
-//                           color: Colors.black,
-//                         ),
-//                       ),
-//                     ),
-//                   ),
-//                 ),
-//               ],
-//             ),
-//             SizedBox(height: 20),
-//             Expanded(
-//               child: SingleChildScrollView(
-//                 child: Padding(
-//                   padding: const EdgeInsets.all(16.0),
-//                   child: Column(
-//                     mainAxisAlignment: MainAxisAlignment.center,
-//                     children: [
-//                       MyCustomForm(),
-//                       SizedBox(height: 20),
-//                       ElevatedButton(
-//                         onPressed: () => navigateToUpdateProfile(context),
-//                         style: ElevatedButton.styleFrom(
-//                           padding: const EdgeInsets.symmetric(
-//                               horizontal: 32.0, vertical: 12.0),
-//                           backgroundColor:
-//                               Colors.green, 
-//                           foregroundColor:
-//                               Colors.white, 
-//                           shape: RoundedRectangleBorder(
-//                             borderRadius: BorderRadius.circular(8.0),
-//                           ),
-//                         ),
-//                         child: const Text('Update Profile'),
-//                       ),
-//                       SizedBox(height: 10),
-//                       ElevatedButton(
-//                         onPressed: () {
-//                           Navigator.push(
-//                             context,
-//                             MaterialPageRoute(
-//                               builder: (BuildContext context) =>
-//                                   const ChangePassword(),
-//                             ),
-//                           );
-//                         },
-//                         style: ElevatedButton.styleFrom(
-//                           padding: const EdgeInsets.symmetric(
-//                               horizontal: 32.0, vertical: 12.0),
-//                           primary:
-//                               Colors.green, // Set the button color to green
-//                           onPrimary:
-//                               Colors.white, // Set the text color to white
-//                           shape: RoundedRectangleBorder(
-//                             borderRadius: BorderRadius.circular(8.0),
-//                           ),
-//                         ),
-//                         child: const Text('Change Password'),
-//                       ),
-//                       SizedBox(height: 10),
-//                     ],
-//                   ),
-//                 ),
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -137,8 +13,8 @@ class RootPage extends StatefulWidget {
 }
 
 class _RootPageState extends State<RootPage> {
-  String? username;
   User? user = Auth().currentUser;
+  String? username;
   String? phoneNumber;
 
   @override
@@ -228,7 +104,6 @@ class _RootPageState extends State<RootPage> {
                     children: [
                       TextFormField(
                         initialValue: username,
-                        readOnly: true,
                         decoration: InputDecoration(
                           icon: Icon(Icons.person, color: Colors.green),
                           labelText: 'Name',
--- lib/pages/taskdoer/taskdoer_homepage.dart
+++ lib/pages/taskdoer/taskdoer_homepage.dart
@@ -60,26 +60,9 @@ class _TaskDoerHomeState extends State<TaskDoerHome> {
       _selectedIndex = index;
     });
 
-    if (_selectedIndex == 1) {
-      _buildSearchBar();
-    }
   }
 
-  Widget _buildSearchBar() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: TextField(
-        decoration: InputDecoration(
-          hintText: 'Search...',
-          prefixIcon: const Icon(Icons.search),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16.0),
-          ),
-          contentPadding: const EdgeInsets.symmetric(vertical: 8.0),
-        ),
-      ),
-    );
-  }
+ 
 
   @override
   Widget build(BuildContext context) {
@@ -184,7 +167,6 @@ class _TaskDoerHomeState extends State<TaskDoerHome> {
       ),
       body: Column(
         children: [
-          _selectedIndex == 0 ? _buildSearchBar() : const SizedBox.shrink(),
           Expanded(
             child: _selectedIndex == 2
                 ? Column(
