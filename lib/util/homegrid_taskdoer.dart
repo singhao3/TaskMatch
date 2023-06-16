b93be060ae2d6287cb43534de81d99c0915d8b61
Lai Chee Yee <cylai2001@graduate.utm.my>
2023-06-16 18:07:45 +0800

    homepage update
    able to open tasklist in homepage

--- lib/util/homegrid_taskdoer.dart
+++ lib/util/homegrid_taskdoer.dart
@@ -1,58 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:taskmatch/util/tasklistscreen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Discoverers extends StatelessWidget {
-  Discoverers({Key? key,required this.document}) : super(key: key);
+  Discoverers({Key? key, required this.document}) : super(key: key);
 
-  final QueryDocumentSnapshot document;
+  final QueryDocumentSnapshot<Object?> document;
 
   @override
   Widget build(BuildContext context) {
     final title = document['title'] ?? '';
     final description = document['description'] ?? '';
-    final budget = document['budget']?.toString() ?? '';
-        return Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Card(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          title,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                          ),
+    final budget = document['budget']?? '';
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => TaskListScreen(document: document),
+            ),
+          );
+        },
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        title,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
                         ),
-                        const SizedBox(height: 4),
-                        Text(
-                          description,
-                          style: const TextStyle(fontSize: 14),
-                        ),
-                        const SizedBox(height: 4),
-                      ],
-                    ),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        description,
+                        style: const TextStyle(fontSize: 14),
+                      ),
+                      const SizedBox(height: 4),
+                    ],
                   ),
-                  Text(
-                    budget,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      color: Colors.green,
-                    ),
+                ),
+                Text(
+                  '\$$budget',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                    color: Colors.green,
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-        );
+        ),
+      ),
+    );
   }
 }
- 
-
